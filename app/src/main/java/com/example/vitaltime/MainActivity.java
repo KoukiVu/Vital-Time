928250883811e6d25bd72fca78ce89a80d2f6db2
pShane05 <05sparish@gmail.com>
2024-07-02 16:43:46 -0400

    Setup menu and icons

--- app/src/main/java/com/example/vitaltime/MainActivity.java
+++ app/src/main/java/com/example/vitaltime/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.vitaltime;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -14,34 +16,32 @@ import com.example.vitaltime.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.google.firebase.FirebaseApp;
+import org.jetbrains.annotations.NotNull;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+     {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
 
+
+
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
