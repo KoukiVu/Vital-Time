018e25a50b170e7ee38e750e1d753b1e162ec41e
Payton De Veyra <deveyrapayton@gmail.com>
2024-07-02 16:57:56 -0400

    Connected JournalHome and MoodTracker

--- app/src/main/java/com/example/vitaltime/FirstFragment.java
+++ app/src/main/java/com/example/vitaltime/FirstFragment.java
@@ -32,7 +32,7 @@ public class FirstFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_moodAndJournal);
+                        .navigate(R.id.action_FirstFragment_to_journalHome);
             }
         });
 
