18cb975c7d0a94e88be41d3fc5340dad3b0d5ae8
Payton De Veyra <deveyrapayton@gmail.com>
2024-07-02 16:00:10 -0400

    Added DiaryEntry and DiaryBook classes

---
+++ app/src/main/java/com/example/vitaltime/DiaryBook.java
@@ -0,0 +1,32 @@
+package com.example.vitaltime;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiaryBook {
+    private Map<Date,DiaryEntry> Diary;
+    public DiaryBook() {
+        Diary = new HashMap<Date,DiaryEntry>();
+
+    }
+    public Map<Date,DiaryEntry> getDiary() {
+        return Diary;
+    }
+    public void addDiaryEntry(DiaryEntry diaryEntry) {
+        Diary.put(diaryEntry.getDate(),diaryEntry);
+    }
+    public void removeDiaryEntry(DiaryEntry diaryEntry) {
+        Diary.remove(diaryEntry.getDate());
+    }
+
+    public DiaryEntry getEntryKey(Date date){
+        for (Map.Entry<Date,DiaryEntry> entry : Diary.entrySet()){
+            Date EntryDate = entry.getKey();
+            if (EntryDate.getMonth() == date.getMonth() && EntryDate.getYear() == date.getYear() && EntryDate.getDate() == date.getDate()){
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+}
