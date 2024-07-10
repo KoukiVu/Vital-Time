d80af551d5eca5116a6e56643ba43755676c7e4a
pShane05 <05sparish@gmail.com>
2024-07-10 17:50:17 -0400

    Dark mode saves state and triggers properly

--- app/src/main/java/com/example/vitaltime/BaseFragment.java
+++ app/src/main/java/com/example/vitaltime/BaseFragment.java
@@ -1,6 +1,8 @@
 package com.example.vitaltime;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.*;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class BaseFragment extends Fragment
 implements  BottomNavigationView.OnNavigationItemSelectedListener {
+    private ThemeViewModel themeViewModel;
     BottomNavigationView bottomNavigationView;
 
     public BaseFragment() {
@@ -30,6 +33,7 @@ implements  BottomNavigationView.OnNavigationItemSelectedListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        themeViewModel = new ViewModelProvider(requireActivity()).get(ThemeViewModel.class);
         setHasOptionsMenu(true);
     }
 
@@ -58,17 +62,33 @@ implements  BottomNavigationView.OnNavigationItemSelectedListener {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_main, menu);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        boolean isDarkTheme = prefs.getBoolean("is_dark_theme", false);
         super.onCreateOptionsMenu(menu, inflater);
+        menu.findItem(R.id.dark_mode).setChecked(isDarkTheme);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        //logout functionality
         if (item.getItemId() == R.id.logout) {
             FirebaseAuth.getInstance().signOut();
             NavHostFragment.findNavController(this).navigate(R.id.loginFragment);
             return true;
-        } else if (item.getItemId() == R.id.dark_mode){
 
+        // dark mode switch functionality
+        } else if (item.getItemId() == R.id.dark_mode){
+            boolean darkMode = !item.isChecked();
+            item.setChecked(darkMode);
+            //applies the appropriate theme based on checkbox
+            if (darkMode) {
+                themeViewModel.setTheme(R.style.Theme_VitalTime_Dark);
+            } else
+                themeViewModel.setTheme(R.style.Theme_VitalTime);
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(requireContext());
+            prefs.edit().putBoolean("is_dark_theme", darkMode).apply();
             return true;
         } else
             return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/vitaltime/MainActivity.java
+++ app/src/main/java/com/example/vitaltime/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.vitaltime;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
@@ -22,19 +25,25 @@ import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity
      {
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-    BottomNavigationView bottomNavigationView;
-    public static SharedPreferences sharedPreferences;
+         private boolean isInitialTrigger = true;
+        private ThemeViewModel themeViewModel;
+        private AppBarConfiguration appBarConfiguration;
+        private ActivityMainBinding binding;
+        BottomNavigationView bottomNavigationView;
+        public static SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Theme_VitalTime);
+        themeViewModel = new ViewModelProvider(this).get(ThemeViewModel.class);
+        themeViewModel.getCurrentTheme().observe(this, this::onThemeChanged);
+        applyTheme();
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
 
 
         setSupportActionBar(binding.toolbar);
@@ -57,5 +66,25 @@ public class MainActivity extends AppCompatActivity
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    private void applyTheme() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isDarkTheme = prefs.getBoolean("is_dark_theme", false);
+        int themeId = isDarkTheme ? R.style.Theme_VitalTime_Dark : R.style.Theme_VitalTime;
+        setTheme(themeId);
+        AppCompatDelegate.setDefaultNightMode(isDarkTheme ?
+                AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
+        themeViewModel.setTheme(themeId);
+    }
+
+         private void onThemeChanged(Integer themeId) {
+             if (isInitialTrigger) {
+                 isInitialTrigger = false;
+                 return;
+             }
+             if (themeId != null) {
+                 recreate();
+             }
+         }
 }
 
