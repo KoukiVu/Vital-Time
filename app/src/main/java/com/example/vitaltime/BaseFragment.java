8bd9be1bc7d276b13e134f3c7d7f05023483ed32
pShane05 <05sparish@gmail.com>
2024-07-11 16:08:44 -0400

    Logout dialog complete

--- app/src/main/java/com/example/vitaltime/BaseFragment.java
+++ app/src/main/java/com/example/vitaltime/BaseFragment.java
@@ -1,5 +1,6 @@
 package com.example.vitaltime;
 
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -74,8 +75,7 @@ implements  BottomNavigationView.OnNavigationItemSelectedListener {
     public boolean onOptionsItemSelected(MenuItem item) {
         //logout functionality
         if (item.getItemId() == R.id.logout) {
-            FirebaseAuth.getInstance().signOut();
-            NavHostFragment.findNavController(this).navigate(R.id.loginFragment);
+            showLogoutDialog(this);
             return true;
 
         // dark mode switch functionality
@@ -95,7 +95,28 @@ implements  BottomNavigationView.OnNavigationItemSelectedListener {
             return super.onOptionsItemSelected(item);
     }
 
-    private void showLogoutDialog() {
+    private void showLogoutDialog(Fragment parent) {
         View dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_logout, null);
+        Button logoutBtn = dialogView.findViewById(R.id.confirm_button);
+        Button cancelBtn = dialogView.findViewById(R.id.cancel_button);
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setView(dialogView);
+        builder.setTitle("Logout");
+        android.app.AlertDialog alertDialog = builder.create();
+        //confirmation logs out user
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                NavHostFragment.findNavController(parent).navigate(R.id.loginFragment);
+                alertDialog.dismiss();}
+        });
+        // cancel closed the dialog
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();}
+        });
+        alertDialog.show();
     }
 }
