ddf9adcbfac07cb2c45fd98051ec6d8413944d86
pShane05 <05sparish@gmail.com>
2024-07-11 16:34:47 -0400

    Merged themes

--- app/src/main/java/com/example/vitaltime/BaseFragment.java
+++ app/src/main/java/com/example/vitaltime/BaseFragment.java
@@ -14,7 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import org.jetbrains.annotations.NotNull;
 
-
+ // commit
 public class BaseFragment extends Fragment
 implements  BottomNavigationView.OnNavigationItemSelectedListener {
     private ThemeViewModel themeViewModel;
